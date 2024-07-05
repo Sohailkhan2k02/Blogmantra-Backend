0a952b01b339f5c644e2f91534fe35a39c34c6b4
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-07-05 18:19:46 +0530

    Delete util directory

--- util/blockUser.js
+++
@@ -1,7 +0,0 @@
-const blockUser = (user)=>{
-    if (user?.isBlocked) {
-        throw new Error(`Access denied ${user?.firstName} is Blocked`)
-    }
-}
-
-module.exports = blockUser;
--- util/validateMongoId.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const validateMongoId = id =>{ 
-    const isValid = mongoose.Types.ObjectId.isValid(id);
-    if(!isValid) throw new Error("Invalid user Id");
-}
-
-module.exports = validateMongoId;
